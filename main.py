94b65e4b149a559d8ce68179e82399994bb86d5d
gerardbill007 <gerard.billington.new@gmail.com>
2025-09-18 21:43:32 +1200

    B. Added clock and code to limit fps to 60.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame
 from constants import *
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    delta_time = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        
+        # Limit the games's fps to 60
+        delta_time = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
