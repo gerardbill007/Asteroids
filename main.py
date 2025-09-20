b03127466b97528989a6739e6e24c1bfe748d759
gerardbill007 <gerard.billington.new@gmail.com>
2025-09-20 14:19:25 +1200

    D. player.py added to the project.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     delta_time = 0
 
     while True:
@@ -13,12 +15,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        # Fill display surface with solid background colour
         screen.fill((0, 0, 0))
+        # Draw player to the screen
+        player.draw(screen)
+        # Funtion to update the entire display screen
         pygame.display.flip()
         
         # Limit the games's fps to 60
         delta_time = clock.tick(60) / 1000
 
-
+        
 if __name__ == "__main__":
     main()
